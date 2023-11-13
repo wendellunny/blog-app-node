98bff8eff2eae4f35ab4e376a156bbdcd9e4dafe
wendellunny <wendelferreiraportugal@gmail.com>
2023-11-13 20:21:07 -0300

    feat: static files

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
 const admin = require('./routes/admin');
+const path = require('path');
 // const mongoose = require('mongoose');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
-app.use('/admin', admin)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/admin', admin);
 
 const PORT=8081;
 app.listen(PORT, () => {
