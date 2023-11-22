c7a9d7badefa33701dd62c6d71f405660da6d51a
wendellunny <wendelferreiraportugal@gmail.com>
2023-11-21 21:59:05 -0300

    feat: validacao de forms

--- routes/admin.js
+++ routes/admin.js
@@ -22,15 +22,38 @@ router.get('/category/add', (req, res) => {
 });
 
 router.post('/category/new', (req, res) => {
+
+    let errors = [];
+
+    if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+         errors.push({text: "Nome inválido"});
+    } 
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({text: "Slug inválido"});
+    }
+
+    if (req.body.name.length < 2) {
+        errors.push({text: "Nome da Categoria é muito pequeno"});
+    }
+
+    if(errors.length > 0) {
+        res.render("admin/addcategory", {errors: errors})
+        return;
+    }
+
+
     const newCategory = {
         name: req.body.name,
         slug: req.body.slug
     };
 
     new Category(newCategory).save().then(()=> {
-        console.log("Categoria salva com sucesso");
+        req.flash('success_msg', "Categoria criada com sucesso");
+        res.redirect('/admin/categories');
     }).catch((err) => {
-        console.log('Erro ao salvar categoria');
+        req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!");
+        res.redirect('/admin/categories');
     });
 });
 
