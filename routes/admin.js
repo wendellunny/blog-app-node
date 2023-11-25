188b7d1fd1831d054084730e89a027304a30aba5
wendellunny <wendelferreiraportugal@gmail.com>
2023-11-24 22:01:15 -0300

    feat: editar categoria

--- routes/admin.js
+++ routes/admin.js
@@ -67,4 +67,31 @@ router.get('/get', (req, res) => {
     res.send('Página de categorias');
 });
 
+router.get("/category/edit/:id", (req, res) => {
+    Category.findOne({_id: req.params.id}).lean().then((category) => {
+        res.render('admin/editCategory', {category: category});
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe");
+        res.redirect("/admin/categories")
+    })
+});
+
+router.post("/category/edit", (req, res) => {
+    Category.findOne({_id: req.body.id}).then((category) => {
+        category.name = req.body.name;
+        category.slug = req.body.slug;
+
+        category.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucessod");
+            res.redirect("/admin/categories");
+        }).catch((err) => {
+            req.flash("error_msg","Houve um erro interno ao salvar a edição da categoria");
+            res.redirect("/admin/categories");
+        });
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar categoria");
+        res.redirect("/admin/categories")
+    })
+});
+
 module.exports = router;
